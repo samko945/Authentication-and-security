a55fbee79dcee1f895cd9e8fe0a20f1a19fa419a
Samuel <samkoj@hotmail.co.uk>
2021-05-22 17:26:24 +0100

    Level 1 security

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+	email: String,
+	password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/home", function (req, res) {
 	res.render("home");
@@ -13,9 +24,35 @@ app.get("/home", function (req, res) {
 app.get("/login", function (req, res) {
 	res.render("login");
 });
+
 app.get("/register", function (req, res) {
 	res.render("register");
 });
+
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save(function(err) {
+        if (!err) {
+            res.render("secrets");
+        }
+    })
+})
+
+app.post("/login", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, doc) {
+        if (doc) {
+            if (doc.password === password) {
+                res.render("secrets");
+            }
+        }
+    }).catch(err => console.error(err));
+})
 
 app.listen(3000, function () {
 	console.log("Server started on port 3000.");
